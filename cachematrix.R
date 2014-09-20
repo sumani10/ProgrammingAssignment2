43f5e5df5f2675c31e068a0ab2c97a51c34c9069
sumani10 <sumani10@gmail.com>
2014-09-20 22:26:59 +0800

    Solved version with comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,15 @@
 ## functions do
 
 ## Write a short comment describing this function
+## The following functions creates inverted matrices with
+## caching for mean calculation.
+## If the mean has been calculated earlier, cached values are returned
+## are not changed.
+
+
+## Creates cacheable matrix for inputting to
+## cacheSolve() function which sets and gets
+## the cached values
 
 makeCacheMatrix <- function(x = matrix()) {
 
